4707fdf6e481e3ac9f4d7644d7955f0c65064731
SaurabhTaroliya <saurabhtaroliya9112001@gmail.com>
2025-03-25 16:20:20 +0530

    Ensures paths are relative during deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./", // Ensures paths are relative during deployment
   build: {
     rollupOptions: {
       external: ["typed.js"], // Exclude 'typed.js' from bundling
